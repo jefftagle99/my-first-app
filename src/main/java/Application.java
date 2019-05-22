ae54658c6ed1bbd6fb74bdd4e7bfda47688f04c2
Jeff Tagle <jefftagle@cocast.net>
2019-05-22 19:59:55 -0400

    Hello Jeff

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String next = scanner.next();
+        System.out.print("Hello " + next);
     }
 }
